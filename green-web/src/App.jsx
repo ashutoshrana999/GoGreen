032ca3af683ebd6dbd9020bd663e4679befc7a5e
Ashutosh Rana <ashutoshrana@ashutoshs-MacBook-Air.local>
2026-09-19 23:27:57 +0530

    update footer

--- green-web/src/App.jsx
+++ green-web/src/App.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Navbar from './component/Navbar'
-import Hero from './component/Hero'
-import Why from './component/Why'
-import Feature from './component/Feature'
-import Favorite from './component/Favorite'
-import About from './component/About'
-import Shop from './component/Shop'
-import Contact from './component/Contact'
-import Productdetails from './component/Productdetails'
-import Cart from './component/Cart'
+import React from "react";
+import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
+import Why from "./component/Why";
+import Feature from "./component/Feature";
+import Favorite from "./component/Favorite";
+import About from "./component/About";
+import Shop from "./component/Shop";
+import Contact from "./component/Contact";
+import Productdetails from "./component/Productdetails";
+import Cart from "./component/Cart";
 import Checkout from "./component/Checkout";
-import { Routes, Route } from 'react-router-dom'
-import Footer from './component/Footer'
-import Login from './component/Login'
-import Signup from './component/Signup'
+import { Routes, Route } from "react-router-dom";
+import Footer from "./component/Footer";
+import Login from "./component/Login";
+import Signup from "./component/Signup";
 import Profile from "./component/Profile";
 
 const Home = () => {
@@ -23,31 +23,36 @@ const Home = () => {
       <Why />
       <Feature />
       <Favorite />
-      <Footer/>
     </>
-  )
-}
+  );
+};
 
 const App = () => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
+
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/product/:id" element={<Productdetails />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/checkout" element={<Checkout />} />
-        
-      </Routes>
-    </>
-  )
-}
+      {/* Main content takes remaining space */}
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/product/:id" element={<Productdetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </main>
 
-export default App
+      <Footer />
+
+    </div>
+  );
+};
+
+export default App;
